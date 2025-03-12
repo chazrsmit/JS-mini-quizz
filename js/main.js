5b0bdc55295ef230888e042022f2f00d9711239b
Charlotte <smit-charlotte@outlook.com>
2025-03-12 14:56:50 +0100

    timeout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 //demander le prénom de l'utilisateur
 
+setTimeout(prenom, 1000) //Timeout pour que le message d'accueil n'apparaisse pas directement
 
+function prenom () {
 let prenom = prompt("Bienvenue ! Quel est ton prénom ?")
+}

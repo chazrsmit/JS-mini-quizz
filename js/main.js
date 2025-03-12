c922fecd1322a13134ae4f848df5ebdf2e32b334
Charlotte <smit-charlotte@outlook.com>
2025-03-12 16:14:09 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-//demander le prénom de l'utilisateur
+// First select the element we want to modify
+let intro = document.querySelector(".mon-texte") //on sélectionne cette partie avant la fonction pour pouvoir modifier le message d'accueil
 
-function prenom () {
-let prenom = prompt("Bienvenue ! Quel est ton prénom ?")
+// Define the message function
+function msg() {
+  var prenom = prompt("Bienvenue ! Quel est ton prénom ?")
+  
+  // Now update the text after we have the prenom value
+  intro.innerText = `Bienvenue ${prenom}` //mettre cette partie dans la fonction sinon on ne peut pas utiliser prénom
 }
 
-setTimeout(prenom, 1000) //Timeout pour que le message d'accueil n'apparaisse pas directement
-
-
-
+// Call the function after a delay
+setTimeout(msg, 1000);

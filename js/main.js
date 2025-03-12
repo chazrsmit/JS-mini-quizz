0eeca7e8c889c1133a8cbd74b969423506aaeab5
Charlotte <smit-charlotte@outlook.com>
2025-03-12 17:23:44 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 // First select the element we want to modify
-let intro = document.querySelector(".mon-texte") //on sélectionne cette partie avant la fonction pour pouvoir modifier le message d'accueil
+// let intro = document.querySelector(".mon-texte") //on sélectionne cette partie avant la fonction pour pouvoir modifier le message d'accueil
 
 // Define the message function
-function msg() {
-  var prenom = prompt("Bienvenue ! Quel est ton prénom ?")
+// function msg() {
+//   var prenom = prompt("Bienvenue ! Quel est ton prénom ?")
   
-  // Now update the text after we have the prenom value
-  intro.innerText = `Bienvenue ${prenom}` //mettre cette partie dans la fonction sinon on ne peut pas utiliser prénom
-}
+//   // Now update the text after we have the prenom value
+//   intro.innerText = `Bienvenue ${prenom}, te voilà dans notre quizz` //mettre cette partie dans la fonction sinon on ne peut pas utiliser prénom
+// }
 
 // Call the function after a delay
-setTimeout(msg, 1000);
+// setTimeout(msg, 1000);
+
+let intro = document.querySelector(".mon-texte")
+let prenom = prompt("Bienvenue ! Quel est ton prénom ?")
+
+intro.innerText = `Bienvenue ${prenom}, te voilà dans notre quizz.`
+
+
+
+
